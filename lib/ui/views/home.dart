0b64b578181fac8f779376d4622cc1cdff7bb855
mac <chisometi@gmail.com>
2021-10-23 11:19:52 +0100

    first commit

--- lib/ui/views/home.dart
+++ lib/ui/views/home.dart
@@ -28,35 +28,40 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     _bookbloc= Provider.of<Bookbloc>(context);
-    return Scaffold(
-     body: SafeArea(
-         child: Padding(
-           padding: EdgeInsets.only(top: 20,left: 20,right: 20),
-           child: Column(
-             crossAxisAlignment: CrossAxisAlignment.start,
-             children: [
-              text('Explore Thousand of books on the go', 25,isbold: true),
-               SizedBox(height: 20,),
-               searchfield(_textEditingController,'search for books...',_bookbloc),
-               SizedBox(height: 20,),
-               text('Famous Books', 20,isbold: true),
-               SizedBox(height: 20,),
-               _bookbloc.books==null?CupertinoActivityIndicator():
-               Expanded(
-                 child: Container(
-                   child: ListView.builder(
-                     itemCount: _bookbloc.books.items.length,
-                       shrinkWrap: true,
-                       physics: BouncingScrollPhysics(),
-                       itemBuilder: (context,index){
-                       var data =_bookbloc.books.items[index];
-                        return bookcard(data,index,context);
-                       }),
-                 ),
-               )
-       ],
-     ),
-         )),
+    return GestureDetector(
+      onTap: (){
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+       body: SafeArea(
+           child: Padding(
+             padding: EdgeInsets.only(top: 20,left: 20,right: 20),
+             child: Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+                text('Explore Thousand of books on the go', 25,isbold: true),
+                 SizedBox(height: 20,),
+                 searchfield(_textEditingController,'search for books...',_bookbloc),
+                 SizedBox(height: 20,),
+                 text('Famous Books', 20,isbold: true),
+                 SizedBox(height: 20,),
+                 _bookbloc.books==null?CupertinoActivityIndicator():
+                 Expanded(
+                   child: Container(
+                     child: ListView.builder(
+                       itemCount: _bookbloc.books.items.length,
+                         shrinkWrap: true,
+                         physics: BouncingScrollPhysics(),
+                         itemBuilder: (context,index){
+                         var data =_bookbloc.books.items[index];
+                          return bookcard(data,index,context);
+                         }),
+                   ),
+                 )
+         ],
+       ),
+           )),
+      ),
     );
   }
 
